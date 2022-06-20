3cfea9178cc0244299d6b773d4ef735666c93dff
Aman Gojariya <amanpatel255144@gmail.com>
2022-06-20 16:38:44 +0530

    theam change

--- seller/src/app/app-routing.module.ts
+++ seller/src/app/app-routing.module.ts
@@ -1,117 +1,33 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-import { DefaultLayoutComponent } from './containers';
-import { Page404Component } from './views/pages/page404/page404.component';
-import { Page500Component } from './views/pages/page500/page500.component';
-import { LoginComponent } from './views/pages/login/login.component';
-import { RegisterComponent } from './views/pages/register/register.component';
+import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './theme/layout/admin/admin.component';
+import {AuthComponent} from './theme/layout/auth/auth.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full'
-  },
-  {
-    path: '',
-    component: DefaultLayoutComponent,
-    data: {
-      title: 'Home'
-    },
+    component: AdminComponent,
     children: [
       {
-        path: 'dashboard',
-        loadChildren: () =>
-          import('./views/dashboard/dashboard.module').then((m) => m.DashboardModule)
+        path: '',
+        redirectTo: 'sample-page',
+        pathMatch: 'full'
       },
       {
-        path: 'orders',
-        loadChildren: () =>
-          import('./views/product-orders/product-orders.module').then((m) => m.ProductOrdersModule)
-      },
-      {
-        path: 'products',
-        loadChildren: () =>
-          import('./views/products/products.module').then((m) => m.ProductsModule)
-      },
-      // {
-      //   path: 'buttons',
-      //   loadChildren: () =>
-      //     import('./views/buttons/buttons.module').then((m) => m.ButtonsModule)
-      // },
-      // {
-      //   path: 'forms',
-      //   loadChildren: () =>
-      //     import('./views/forms/forms.module').then((m) => m.CoreUIFormsModule)
-      // },
-      // {
-      //   path: 'charts',
-      //   loadChildren: () =>
-      //     import('./views/charts/charts.module').then((m) => m.ChartsModule)
-      // },
-      // {
-      //   path: 'icons',
-      //   loadChildren: () =>
-      //     import('./views/icons/icons.module').then((m) => m.IconsModule)
-      // },
-      // {
-      //   path: 'notifications',
-      //   loadChildren: () =>
-      //     import('./views/notifications/notifications.module').then((m) => m.NotificationsModule)
-      // },
-      // {
-      //   path: 'widgets',
-      //   loadChildren: () =>
-      //     import('./views/widgets/widgets.module').then((m) => m.WidgetsModule)
-      // },
-      {
-        path: 'pages',
-        loadChildren: () =>
-          import('./views/pages/pages.module').then((m) => m.PagesModule)
-      },
+        path: 'sample-page',
+        loadChildren: () => import('./demo/pages/sample-page/sample-page.module').then(module => module.SamplePageModule)
+      }
     ]
   },
   {
-    path: '404',
-    component: Page404Component,
-    data: {
-      title: 'Page 404'
-    }
-  },
-  {
-    path: '500',
-    component: Page500Component,
-    data: {
-      title: 'Page 500'
-    }
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-    data: {
-      title: 'Login Page'
-    }
-  },
-  {
-    path: 'register',
-    component: RegisterComponent,
-    data: {
-      title: 'Register Page'
-    }
-  },
+    path: '',
+    component: AuthComponent,
+    children: []
+  }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, {
-      scrollPositionRestoration: 'top',
-      anchorScrolling: 'enabled',
-      initialNavigation: 'enabledBlocking'
-      // relativeLinkResolution: 'legacy'
-    })
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
