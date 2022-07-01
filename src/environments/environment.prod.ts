46fb0e2e24e98168f9268f5fc59552c0afe2bc63
sumit75757 <sumitpatel75757@gmail.com>
2022-07-01 17:24:31 +0530

    first commit

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  // baseUrl: "http://localhost:4000/",
-  //   imageURL: "http://localhost:4000"
-  imageURL: "https://nodejssssss.herokuapp.com",
-
-  baseUrl: "https://nodejssssss.herokuapp.com/"
-};
