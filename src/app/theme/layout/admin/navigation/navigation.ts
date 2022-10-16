0f3907219c516538966878c82a26e77ad56b8c9a
sumit75757 <sumitpatel75757@gmail.com>
2022-10-16 10:53:27 +0530

    update

--- src/app/theme/layout/admin/navigation/navigation.ts
+++ src/app/theme/layout/admin/navigation/navigation.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 
 export interface NavigationItem {
   id: string;
@@ -23,80 +23,219 @@ export interface NavigationItem {
 
 export interface Navigation extends NavigationItem {
   children?: NavigationItem[];
+
 }
 
-const NavigationItems = [
-  {
-    id: 'other',
-    title: 'Admin',
-    type: 'group',
-    icon: 'feather icon-align-left',
-    children: [
-      {
-        id: 'sample-page',
-        title: 'Sample Page',
-        type: 'item',
-        url: '/sample-page',
-        classes: 'nav-item',
-        icon: 'feather icon-sidebar'
-      }, 
-      {
-        id: 'sellers',
-        title: 'Shops',
-        type: 'item',
-        url: '/seller',
-        classes: 'nav-item',
-        icon: 'feather icon-users',
-         children: [
-          {
-             id: 'product',
-            title: 'Product',
-            type: 'item',
-             url: '/seller/prodects/'
-          },
-        ]
-      },
-      {
-        id: 'service',
-        title: 'Service',
-        type: 'item',
-        url: '/service',
-        classes: 'nav-item',
-        icon: 'feather icon-settings'
-      },
-      {
-        id: 'service catogary',
-        title: 'Service Catogary',
-        type: 'item',
-        url: '/services/catogory',
-        classes: 'nav-item',
-        icon: 'feather icon-settings'
-      },
-      {
-        id: 'catogorys',
-        title: 'Catogorys',
-        type: 'item',
-        url: '/catogory',
-        classes: 'nav-item',
-        icon: 'feather icon-grid'
-      },
-      {
-        id: 'users',
-        title: 'Users',
-        type: 'item',
-        url: '/users',
-        classes: 'nav-item',
-        icon: 'feather icon-user'
-      },
+
+// const NavigationItems = [
+//   {
+//     id: 'other',
+//     title: 'Admin',
+//     type: 'group',
+//     icon: 'feather icon-align-left',
+//     children: [
+//       {
+//         id: 'sample-page',
+//         title: 'Sample Page',
+//         type: 'item',
+//         url: '/sample-page',
+//         classes: 'nav-item',
+//         icon: 'feather icon-sidebar'
+//       },
+//       {
+//         id: 'sellers',
+//         title: 'Shops',
+//         type: 'item',
+//         url: '/seller',
+//         classes: 'nav-item',
+//         icon: 'feather icon-users',
+//          children: [
+//           {
+//              id: 'product',
+//             title: 'Product',
+//             type: 'item',
+//              url: '/seller/prodects/'
+//           },
+//         ]
+//       },
+//       {
+//         id: 'service',
+//         title: 'Service',
+//         type: 'item',
+//         url: '/service',
+//         classes: 'nav-item',
+//         icon: 'feather icon-settings'
+//       },
+//       {
+//         id: 'service catogary',
+//         title: 'Service Catogary',
+//         type: 'item',
+//         url: '/services/catogory',
+//         classes: 'nav-item',
+//         icon: 'feather icon-settings'
+//       },
+//       {
+//         id: 'catogorys',
+//         title: 'Catogorys',
+//         type: 'item',
+//         url: '/catogory',
+//         classes: 'nav-item',
+//         icon: 'feather icon-grid'
+//       },
+//       {
+//         id: 'users',
+//         title: 'Users',
+//         type: 'item',
+//         url: '/users',
+//         classes: 'nav-item',
+//         icon: 'feather icon-user'
+//       },
 
 
-    ]
-  }
-];
+//     ]
+//   }
+// ];
 
 @Injectable()
 export class NavigationItem {
-  public get() {
+  navigation() {
+
+    let NavigationItems
+    let userType: any = JSON.parse (localStorage.getItem('userData'))
+
+    if (userType.character === 'Admin') {
+      NavigationItems = [
+        {
+          id: 'other',
+          title: 'Admin',
+          type: 'group',
+          icon: 'feather icon-align-left',
+          children: [
+            {
+              id: 'sample-page',
+              title: 'Sample Page',
+              type: 'item',
+              url: '/sample-page',
+              classes: 'nav-item',
+              icon: 'feather icon-sidebar'
+            },
+            {
+              id: 'sellers',
+              title: 'Shops',
+              type: 'item',
+              url: '/seller',
+              classes: 'nav-item',
+              icon: 'feather icon-users',
+              children: [
+                {
+                  id: 'product',
+                  title: 'Product',
+                  type: 'item',
+                  url: '/seller/prodects/'
+                },
+              ]
+            },
+            {
+              id: 'service',
+              title: 'Service',
+              type: 'item',
+              url: '/service',
+              classes: 'nav-item',
+              icon: 'feather icon-settings'
+            },
+            {
+              id: 'service catogary',
+              title: 'Service Catogary',
+              type: 'item',
+              url: '/services/catogory',
+              classes: 'nav-item',
+              icon: 'feather icon-settings'
+            },
+            {
+              id: 'catogorys',
+              title: 'Catogorys',
+              type: 'item',
+              url: '/catogory',
+              classes: 'nav-item',
+              icon: 'feather icon-grid'
+            },
+            {
+              id: 'users',
+              title: 'Users',
+              type: 'item',
+              url: '/users',
+              classes: 'nav-item',
+              icon: 'feather icon-user'
+            },
+
+
+          ]
+        }
+      ];
+    } else if (userType.character === 'seller') {
+      NavigationItems = [
+        {
+          id: 'other',
+          title: 'Admin',
+          type: 'group',
+          icon: 'feather icon-align-left',
+          children: [
+            {
+              id: 'sample-page',
+              title: 'Sample Page',
+              type: 'item',
+              url: '/sample-page',
+              classes: 'nav-item',
+              icon: 'feather icon-sidebar'
+            },
+            {
+              id: 'sellers',
+              title: 'Shops',
+              type: 'item',
+              url: '/seller',
+              classes: 'nav-item',
+              icon: 'feather icon-users',
+              children: [
+                {
+                  id: 'product',
+                  title: 'Product',
+                  type: 'item',
+                  url: '/seller/prodects/'
+                },
+              ]
+            },
+            {
+              id: 'service',
+              title: 'Service',
+              type: 'item',
+              url: '/service',
+              classes: 'nav-item',
+              icon: 'feather icon-settings'
+            },
+            {
+              id: 'users',
+              title: 'Users',
+              type: 'item',
+              url: '/users',
+              classes: 'nav-item',
+              icon: 'feather icon-user'
+            },
+
+
+          ]
+        }
+      ];
+
+    }
+
+    console.log(userType);
+
     return NavigationItems;
+  }
+  public get() {
+
+    console.log(this.navigation());
+
+    return this.navigation();
   }
 }
